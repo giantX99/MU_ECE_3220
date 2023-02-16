50cc09108311c01120292f8c8b6aee12c852a49b
giantX99 <93807005+giantX99@users.noreply.github.com>
2023-02-16 11:42:17 -0600

    changes

---
+++ cpp_lab4_examp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+
+class MyClass{
+//private by default
+    constructor //creates an instance of the class
+    destructor //Cleans up the the instance created
+};
+
+struct MyStruct{
+//public by default
+};
